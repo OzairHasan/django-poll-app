4aeedbfd5ce3cb329ab9ae62fbf04900abf2cb09
OzairHasan <e0407597@u.nus.edu>
2022-10-12 02:44:20 +0800

    create view templates and update views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from polls.models import Question
 from django.utils import timezone
-# Create your views here.
+from django.template import loader
+from django.shortcuts import render
 
 def questionCompareByPubDate(question):
     return question.pub_date
@@ -10,10 +12,19 @@ def questionCompareByPubDate(question):
 def index(request):
     questions = Question.objects.all()
     questions = sorted(questions, key=questionCompareByPubDate)
-    return HttpResponse(questions)
+
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': questions,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
